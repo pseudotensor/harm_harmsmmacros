af62495a1366d0dfcef5d267ff272c195fc1ef2e
Jonathan McKinney <pseudotensor@users.noreply.github.com>
2010-04-15 04:37:25 +0000

    thickdisk

---
+++ thickdisk.m
@@ -0,0 +1,20 @@
+getmacros 0     #
+		gogrmhd
+		jre mri.m
+		#
+dothick1 0      #
+		#
+		#
+		jrdpcf3duentropy dump0000
+		#
+		mricalc
+		fieldcalc 0 aphi
+		#
+		plc 0 (rho-1)
+		#plc 0 aphi 010
+		plc 0 ibeta 010
+		#
+		#
+		plc 0 (rho-1)
+		plc 0 idx2mri 010
+		#
